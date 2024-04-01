b007f0e83ae8e1331cdbf3281b24a1b44163d218
Simon <simon.ciperle@gmail.com>
2024-04-01 22:21:02 +0200

    Upload controller, validate, save to db and return response

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UploadResource;
+use App\Models\UploadedFile;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\ValidationException;
+
+class UploadController extends Controller
+{
+    public function Upload(Request $request) {
+
+        $fileRequestKey = 'file';
+        $maxSize = 10240;
+
+        try {
+            $request->validate([
+                $fileRequestKey => 'required|file|mimes:jpeg,png,mp4,svg|max:'.$maxSize,
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'There was error in process of uploading file. Check the messages for details!',
+                'errors' => $e->errors(),
+            ], 422);
+        }
+
+        $file = $request->file($fileRequestKey);
+        $fileName = Carbon::now()->format('Ymd-His').'_'.$file->getClientOriginalName();
+
+        $newFile = new UploadedFile();
+        $newFile->original_name = $file->getClientOriginalName();
+        $newFile->file_name = $fileName;
+        $newFile->file_path = Storage::path($file->storeAs('upload', $fileName));
+        $newFile->file_size = $file->getSize();
+        $newFile->mime_type = $file->getClientMimeType();
+        $newFile->extension =  $file->getClientOriginalExtension();
+        if ($newFile->save()) {
+            return new UploadResource($newFile);
+        }
+        else {
+            return response()->json(['message'=> 'An error occurred!'],500);
+        }
+    }
+
+}
