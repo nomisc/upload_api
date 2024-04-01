dd0e62ab61998f366fcf19a2ca27e0fde1f47dbb
Simon <simon.ciperle@gmail.com>
2024-04-01 23:10:43 +0200

    Custom middleware, check for token, register middleware

---
+++ app/Http/Middleware/CustomApiAuth.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomApiAuth
+{
+
+    private array $keys;
+
+    public function __construct()
+    {
+        $this->keys = explode('|',config('app.api_keys'));
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if (!in_array($request->header('ApiToken'),$this->keys)) {
+            return response()->json(['message'=>'credential error'],401);
+        }
+
+        return $next($request);
+    }
+}
